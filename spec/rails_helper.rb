8039ced94cd8efa8e991d715ec87f4c50c2ad1f4
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-09-02 23:10:16 +0200

    Shoulda and FactoryBot setup in RSpec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,19 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  # To use 'should' and 'expect' without deprecated error.
+  config.expect_with :rspec do |c|
+    c.syntax = %i[should expect]
+  end
+
+  Shoulda::Matchers.configure do |conf|
+    conf.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  config.include Shoulda::Callback::Matchers::ActiveModel
 end
