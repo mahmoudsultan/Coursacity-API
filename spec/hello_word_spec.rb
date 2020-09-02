d8107b7e182794d3175913e65d46dde598f36414
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-09-02 23:11:17 +0200

    Dummy Test Spec.

---
+++ spec/hello_word_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Hello, World' do
+  it 'succeeds' do
+    dummy_variable = true
+    expect(dummy_variable).to be_truthy
+  end
+end
