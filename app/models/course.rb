f2c1c5dc05083b01dd5d777c9f496622d0dc4605
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-09-03 19:27:43 +0200

    Course Model Validations.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+  validates :title, presence: true, length: { in: 3..100 }
+  validates :description, presence: true
+  validates :slug, presence: true, length: { in: 3..100 }
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_length_of(:title).is_at_least(3).is_at_most(100) }
+
+    it { should validate_presence_of :description }
+
+    it { should validate_presence_of :slug }
+    it { should validate_length_of(:slug).is_at_least(3).is_at_most(100) }
+  end
 end
